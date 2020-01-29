4bfcc217ea6c839c8f83185e0a9f4be726e7d8fd
Leonardo <leeonardo.alves@hotmail.com>
2020-01-29 12:06:21 -0300

    comming and about

--- src/app/site/pages/about-us/about-us.component.ts
+++ src/app/site/pages/about-us/about-us.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutUsComponent implements OnInit {
   banner: Object[];
-
+  panelOpenState: boolean;
   constructor() {
     this.banner = [
       {
